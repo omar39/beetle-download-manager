cb3b537050d99bd5ace409b8aa8917da94c4a168
Omar Khaled <omar4550@live.com>
2019-05-05 16:22:55 +0200

    zula's habd

--- src/Classes/InProgressDownload.java
+++ src/Classes/InProgressDownload.java
@@ -3,6 +3,8 @@ package Classes;
 import java.io.*;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
+
 public class InProgressDownload {
     private File file;
     private double currentSize;
@@ -16,26 +18,45 @@ public class InProgressDownload {
         
         return currentSize;
     }
-   void llaa()
-   {
-       String FILE_URL = null;
-       try(BufferedInputStream in = new BufferedInputStream(new URL(FILE_URL).openStream()))
-        {
-            String fileName = FILE_URL.substring(FILE_URL.lastIndexOf('/')+1, FILE_URL.length());
-            System.out.println(fileName);
-            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-                byte dataBuffer[] = new byte[1024];
-                int bytesRead;
-                
-                while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                fileOutputStream.write(dataBuffer, 0, bytesRead);
-                
-            }  
+   public void downloadFile(String fileURL, String saveDir) throws IOException {
+        
+       long downloadedLength = 0;
+        saveDir += fileURL.substring(fileURL.lastIndexOf('/')+1, fileURL.length());
+        java.io.File file = new java.io.File(saveDir);
+        URL url = new URL(fileURL);
+ 
+        BufferedInputStream inputStream = null;
+        BufferedOutputStream outputStream = null;
+        saveDir += fileURL.substring(fileURL.lastIndexOf('/')+1, fileURL.length());
+
+        URLConnection connection = url.openConnection();
+        
+        if(file.exists()){
+            downloadedLength = file.length();
+            connection.setRequestProperty("Range", "bytes=" + downloadedLength + "-");
+            outputStream = new BufferedOutputStream(new FileOutputStream(file, true));
+
+        }else{
+            outputStream = new BufferedOutputStream(new FileOutputStream(file));
         }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-}
+
+        connection.connect();
+
+        inputStream = new BufferedInputStream(connection.getInputStream());
+
+
+        byte[] buffer = new byte[1024*8];
+        int byteCount;
+
+       BufferedInputStream bis = new BufferedInputStream(url.openStream());
+            FileOutputStream fis = new FileOutputStream(file);
+            int count = 0;
+            while ((count = bis.read(buffer, 0, 1024)) != -1)
+            {
+                fis.write(buffer, 0, count);           
+            }
+            bis.close();
+            fis.close(); 
+    }
 }
 
